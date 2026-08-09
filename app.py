0a4cb9bf56a5a81885a1bdb3fce7fdbf9b5bd827
roshini1803 <yourgithubemail@gmail.com>
2026-08-09 10:35:36 +0530

    feat: containerize smart parking application with Docker

--- app.py
+++ app.py
@@ -231,5 +231,8 @@ def reset():
 # RUN APP
 # ==========================
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
